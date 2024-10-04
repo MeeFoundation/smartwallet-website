c7ef78b6007caaa2722e1f2362f17f13e3cc87f9
Raman Siliuk <rsiluk@swiftinvention.com>
2024-10-04 15:49:52 +0400

    added custom Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -76,6 +76,7 @@ export default defineConfig({
         EditLink: "./src/components/starlight/EditLink.astro",
         Header: "./src/components/starlight/DocsHead.astro",
         PageFrame: "./src/components/starlight/PageFrame.astro",
+        Footer: "./src/components/starlight/Footer.astro",
       },
     }),
     tailwind(),
