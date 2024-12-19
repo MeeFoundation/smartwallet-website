150e7210df777d5a56e6f11c95234bed02b8d726
Raman Siliuk <rsiluk@swiftinvention.com>
2024-12-19 16:43:34 +0400

    mee-components updated, astro updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import starlight from "@astrojs/starlight";
-
 export const site = "https://smartwallet.mee.foundation/";
-export const meeFoundation = "https://mee.foundation/";
-export const github = "https://github.com/MeeFoundation/smartwallet-website";
-export const discord = "https://discord.gg/PB8qxrQXup";
 export const sharedConfig = {
   title: "Mee Smartwallet",
   logo: {
@@ -15,99 +10,10 @@ export const sharedConfig = {
     replacesTitle: true,
   },
 };
-export const header = {
-  logoLink: meeFoundation,
-  links: [
-    {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
-    },
-    {
-      name: "Businesses",
-      link: `${meeFoundation}businesses/`,
-      id: "businesses",
-    },
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-  ],
-  rightLinks: [
-    {
-      name: "Join us",
-      link: discord,
-      icon: "discord",
-      target: "_blank",
-      id: "discord",
-    },
-  ],
-  footerLinks: [
-    {
-      link: `${meeFoundation}privacy-policy/`,
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-    {
-      link: "/docs/",
-      name: "Docs",
-      id: "docs",
-    },
-  ],
-};
-export const footer = {
-  links: [
-    {
-      link: "https://x.com/mee_foundation",
-      icon: "twitter-x",
-      target: "_blank",
-    },
-    {
-      link: github,
-      icon: "github",
-      target: "_blank",
-    },
-    {
-      link: discord,
-      icon: "discord",
-      target: "_blank",
-    },
-  ],
-  rightLinks: [
-    {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
-    },
-    {
-      name: "Businesses",
-      link: `${meeFoundation}businesses/`,
-      id: "businesses",
-    },
-    {
-      name: "About us",
-      link: `${meeFoundation}about/`,
-      id: "about",
-    },
-    {
-      link: `${meeFoundation}privacy-policy/`,
-      name: "Privacy",
-      id: "privacy-policy",
-    },
-    {
-      link: "/docs/",
-      name: "Docs",
-    },
-  ],
-};
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
   site: site,
   integrations: [
     starlight({
@@ -121,9 +27,6 @@ export default defineConfig({
       editLink: {
         baseUrl:
           "https://github.com/meefoundation/smartwallet-website/edit/main/",
-      },
-      social: {
-        github: github,
       },
       customCss: process.env.NO_GRADIENTS
         ? []
--- src/config.ts
+++ src/config.ts
@@ -1,41 +1,135 @@
+export const meeFoundation = "https://mee.foundation/";
+export const github = "https://github.com/MeeFoundation/smartwallet-website";
+export const discord = "https://discord.gg/PB8qxrQXup";
+
+export const header = {
+  logoLink: meeFoundation,
+  links: [
+    {
+      name: "Projects",
+      link: `${meeFoundation}projects/`,
+      id: "projects",
+    },
+    {
+      name: "Businesses",
+      link: `${meeFoundation}businesses/`,
+      id: "businesses",
+    },
+    {
+      name: "About us",
+      link: `${meeFoundation}about/`,
+      id: "about",
+    },
+  ],
+  rightLinks: [
+    {
+      name: "Join us",
+      link: discord,
+      icon: "discord",
+      target: "_blank",
+      id: "discord",
+    },
+  ],
+  footerLinks: [
+    {
+      link: `${meeFoundation}privacy-policy/`,
+      name: "Privacy",
+      id: "privacy-policy",
+    },
+    {
+      link: "/docs/",
+      name: "Docs",
+      id: "docs",
+    },
+  ],
+};
+export const footer = {
+  links: [
+    {
+      link: "https://x.com/mee_foundation",
+      icon: "twitter-x",
+      target: "_blank",
+    },
+    {
+      link: github,
+      icon: "github",
+      target: "_blank",
+    },
+    {
+      link: discord,
+      icon: "discord",
+      target: "_blank",
+    },
+  ],
+  rightLinks: [
+    {
+      name: "Projects",
+      link: `${meeFoundation}projects/`,
+      id: "projects",
+    },
+    {
+      name: "Businesses",
+      link: `${meeFoundation}businesses/`,
+      id: "businesses",
+    },
+    {
+      name: "About us",
+      link: `${meeFoundation}about/`,
+      id: "about",
+    },
+    {
+      link: `${meeFoundation}privacy-policy/`,
+      name: "Privacy",
+      id: "privacy-policy",
+    },
+    {
+      link: "/docs/",
+      name: "Docs",
+    },
+  ],
+};
 export type EnvType = "local" | "prod";
 
 export const getAppEnv = (): EnvType => {
+  if (typeof window !== "undefined") {
     const currentHost = window.location.hostname;
     if (/.*localhost.*/.test(currentHost)) {
-        return "local";
+      return "local";
     } else {
-        return "prod";
+      return "prod";
     }
+  } else {
+    return "prod";
+  }
 };
 
 export interface AppConfig {
-    type: "local" | "prod";
-    backendUrl: string;
+  type: "local" | "prod";
+  backendUrl: string;
 }
 
 const localConfig: AppConfig = {
-    type: "local",
-    backendUrl: "http://localhost:8000",
+  type: "local",
+  backendUrl: "http://localhost:8000",
 };
 
 const prodConfig: AppConfig = {
-    type: "prod",
-    backendUrl: "http://localhost:8000",
+  type: "prod",
+  backendUrl: "http://localhost:8000",
 };
 
 const getConfig = (): AppConfig => {
-    const env = getAppEnv();
-    switch (env) {
-        case "local":
-            return localConfig;
-        case "prod":
-            return prodConfig;
-        default:
-            throw new Error("Unknown env was used in app configuration");
-    }
+  const env = getAppEnv();
+  switch (env) {
+    case "local":
+      return localConfig;
+    case "prod":
+      return prodConfig;
+    default:
+      throw new Error("Unknown env was used in app configuration");
+  }
 };
 
 export default {
-    ...getConfig(),
+  ...getConfig(),
 };
